98cfd23a0b65af0e8649ad576f1ea137c7a2ffc7
le50 <leti.tec1000@gmail.com>
2025-09-19 10:32:31 -0300

    Adicionado stters

--- src/Discente.java
+++ src/Discente.java
@@ -7,12 +7,36 @@ public class Discente extends Pessoa{
 	int nota3At;
 	int nota4At;
 	
-	public Discente(int nota1Par, int nota2Par, int nota3Par, int nota4Par) {
-		super();
+	//Construtores:
+	public Discente(int nota1Par, int nota2Par, int nota3Par, int nota4Par, String nomePar, int idadePar, Endereco enderecoPar) {
+		
+		super(nomePar, idadePar, enderecoPar);
 		this.nota1At = nota1Par;
 		this.nota2At = nota2Par;
 		this.nota3At = nota3Par;
 		this.nota4At = nota4Par;
 
+	}
+	
+	//Setter:
+	public void setNota1(int nota) {
+		this.nota1At = nota;
+	}
+	
+	public void setNota2(int nota) {
+		this.nota2At = nota;
+	}
+	
+	public void setNota3(int nota) {
+		this.nota3At = nota;
+	}
+	
+	public void setNota4(int nota) {
+		this.nota4At = nota;
+	}
+	
+	// getters
+	public int getNota1() {
+		
 	}
 }
