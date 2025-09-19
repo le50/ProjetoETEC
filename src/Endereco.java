70891fc83b9e9c57ea2d0d6c29082550fa7cb342
le50 <leti.tec1000@gmail.com>
2025-09-19 09:37:50 -0300

    Adicionado conteudo

--- src/Endereco.java
+++ src/Endereco.java
@@ -2,7 +2,28 @@
 public class Endereco {
 
 	// Abaixo da class = atributo
+	// Atributos
 	String ruaAt;
 	String cidadeAt;
 	String estadoAt;
+	
+	// Construtores:
+	public Endereco(String ruaPar, String cidadePar, String estadoPar) {
+		
+		this.ruaAt = ruaPar;
+		this.cidadeAt = cidadePar;
+		this.estadoAt = estadoPar;
+		
+	}
+	
+	// Getters & Setters:
+	// Setters
+	public void setRua(String ruaPar) {
+		this.ruaAt = ruaPar;
+		
+	}
+	
+	public void String getRua() {
+		return
+	}
 }
