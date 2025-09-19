102a7d87cae006d8c5ffca6ccf0e315aa41e687c
le50 <leti.tec1000@gmail.com>
2025-09-19 10:21:51 -0300

    Adicionado discente

--- src/Discente.java
+++ src/Discente.java
@@ -1,4 +1,18 @@
 
-public class Discente {
+public class Discente extends Pessoa{
 
+	// Atributos
+	int nota1At;
+	int nota2At;
+	int nota3At;
+	int nota4At;
+	
+	public Discente(int nota1Par, int nota2Par, int nota3Par, int nota4Par) {
+		super();
+		this.nota1At = nota1Par;
+		this.nota2At = nota2Par;
+		this.nota3At = nota3Par;
+		this.nota4At = nota4Par;
+
+	}
 }
